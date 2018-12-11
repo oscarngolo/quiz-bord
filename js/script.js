c4fec97bbb94bb2801d7d2627743a758725de89d
oscar ngolo <oscarngoloman@yahoo.com>
2018-12-11 08:11:01 +0300

    add all changes made

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ $(document).ready(function() {
       if (currentquestion == allQuestions.length - 1) {
         $('#next').html("Submit");
         $('#next').click(function() {
-          $("#result").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
+          $("#result").html("You correctly answered  " + correctAnswers * 20 + " out of " + currentquestion * 20).hide();
           $("#result").fadeIn();
         });
 
